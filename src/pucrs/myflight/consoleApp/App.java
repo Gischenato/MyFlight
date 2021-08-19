2552c857b6b7850d96ff613d878f7c5c84b402d9
Giovanni Schenato <gischenato@gmail.com>
2021-08-19 16:28:09 -0300

    GerenciadorCias

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -1,7 +1,9 @@
 package pucrs.myflight.consoleApp;
 
 import pucrs.myflight.modelo.Aeronave;
+import pucrs.myflight.modelo.CiaAerea;
 import pucrs.myflight.modelo.GerenciadorAeronaves;
+import pucrs.myflight.modelo.GerenciadorCias;
 
 public class App {
 
@@ -9,26 +11,48 @@ public class App {
 		System.out.println("\nMyFlight project...");
 
 		GerenciadorAeronaves aeronaves = new GerenciadorAeronaves();
+		GerenciadorCias empresas = new GerenciadorCias();
 
-		Aeronave a1 = new Aeronave("122", "nave1", 144);
-		Aeronave a2 = new Aeronave("123", "nave2 testada", 144);
-		Aeronave a3 = new Aeronave("124", "nave3", 144);
-
+		Aeronave a1 = new Aeronave("733", "Boeing 737-300", 140);
+		Aeronave a2 = new Aeronave("73G", "Boeing 737-700", 126);
+		Aeronave a3 = new Aeronave("380", "Airbus Industrie A380", 644);
+		Aeronave a4 = new Aeronave("764", "Boeing 767-400", 304);
 		aeronaves.adicionar(a1);
 		aeronaves.adicionar(a2);
 		aeronaves.adicionar(a3);
+		aeronaves.adicionar(a4);
 
-		System.out.println(aeronaves.listarTodas());
+		CiaAerea c1 = new CiaAerea("JJ", "LATAM Linhas Aereas");
+		CiaAerea c2 = new CiaAerea("G3", "Gol Linhas Aereas SA");
+		CiaAerea c3 = new CiaAerea("TP", "TAP Portugal");
+		CiaAerea c4 = new CiaAerea("AD", "Azul Linhas Aereas");
+		empresas.adicionar(c1);
+		empresas.adicionar(c2);
+		empresas.adicionar(c3);
+		empresas.adicionar(c4);
 
-		String cod = "122";
+		// System.out.println(aeronaves.listarTodas());
 
-		Aeronave teste = aeronaves.buscarPorCodigo(cod);
+		// String codAeronave = "380";
 
-		if(teste != null){
-			System.out.println("\n" + teste.getDescricao());
-		}else{
-			System.out.printf("Aeronave com codigo %s nao encontrado\n", cod);
-		}
+		// Aeronave teste1 = aeronaves.buscarPorCodigo(codAeronave);
+
+		// if(teste != null){
+		// 	System.out.println("\n" + teste.getDescricao());
+		// }else{
+		// 	System.out.printf("Aeronave com codigo %s nao encontrado\n", cod);
+		// }
+
+		System.out.println(empresas.listarTodas());
+
+		String codEmpresa = "G2";
+		String nomeEmpresa = "Azul Linhas Aereas";
+
+		CiaAerea teste2 = empresas.buscarPorCodigo(codEmpresa);
+		CiaAerea teste3 = empresas.buscarPorNome(nomeEmpresa);
+
+		System.out.println(teste2 == null?("Empresa com codigo " + codEmpresa + " nao encontrado") : teste2.getNome());
+		System.out.println(teste3 == null?("Empresa com nome " + nomeEmpresa + " nao encontrado") : teste3.getNome());
 
 	}
 }
--- src/pucrs/myflight/modelo/GerenciadorCias.java
+++ src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -8,4 +8,36 @@ public class GerenciadorCias {
 	public GerenciadorCias() {
 		empresas = new ArrayList<>();
 	}
+
+	public void adicionar(CiaAerea cia){
+		empresas.add(cia);
+	}
+
+	public String listarTodas(){
+		String res = "Empresas:\n";
+
+		for(int i = 0; i<empresas.size(); i++){
+			CiaAerea atual = empresas.get(i);
+			res += "  ("+ atual.getCodigo() + ") - " + atual.getNome() +"\n";
+		}
+
+		return res;
+	}
+
+	public CiaAerea buscarPorCodigo(String cod){
+		for(int i = 0; i<empresas.size(); i++){
+			CiaAerea atual = empresas.get(i);
+			if(cod == atual.getCodigo()){ return atual; }
+		}
+
+		return null;
+	}
+
+	public CiaAerea buscarPorNome(String nome){
+		for(int i = 0; i<empresas.size(); i++){
+			CiaAerea atual = empresas.get(i);
+			if(nome == atual.getNome()){ return atual; }
+		}
+		return null;
+	}
 }
